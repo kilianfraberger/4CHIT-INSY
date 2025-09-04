8ad20b4909cd64a81d39657a4dee505f6301e5c9
kilianfraberger <k.fraberger@htlkrems.at>
2025-09-04 10:22:23 +0200

    DB Search Speed

---
+++ DB_Search_Speed/DB_Search_Speed/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = @"server=localhost;userid=root;password=insy;database=DB_Search_Speed";
+        int searchCount = 100;
+        Random rnd = new Random();
+
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+            Console.WriteLine("Starte Suche...");
+            Stopwatch sw = Stopwatch.StartNew();
+            int foundCount = 0;
+
+            using (MySqlCommand cmd = new MySqlCommand("SELECT COUNT(*) FROM Words WHERE value = @val", conn))
+            {
+                var param = cmd.Parameters.Add("@val", MySqlDbType.VarChar);
+
+                for (int i = 0; i < searchCount; i++)
+                {
+                    string word = GenerateRandomWord(10, rnd);
+                    param.Value = word;
+                    long count = (long)cmd.ExecuteScalar();
+                    if (count > 0) foundCount++;
+
+                    Console.WriteLine($"Suche {i + 1}/{searchCount}: {word} {(count > 0 ? "gefunden" : "nicht gefunden")}");
+                }
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Suche abgeschlossen. {foundCount} von {searchCount} Wörtern gefunden.");
+            Console.WriteLine($"Benötigte Zeit: {sw.Elapsed.TotalMilliseconds:N0} ms");
+        }
+    }
+
+    static string GenerateRandomWord(int length, Random rnd)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++)
+        {
+            sb.Append(chars[rnd.Next(chars.Length)]);
+        }
+        return sb.ToString();
+    }
+}
---
+++ DB_Search_Speed/DB_Search_Speed_Einfügen/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string connectionString = @"server=localhost;userid=root;password=insy;database=DB_Search_Speed";
+        int wordCount = 1_000_000;
+        int batchSize = 10_000;
+        Random rnd = new Random();
+
+        using (MySqlConnection conn = new MySqlConnection(connectionString))
+        {
+            conn.Open();
+
+            Console.WriteLine("Leere Tabelle...");
+            using (var cmd = new MySqlCommand("TRUNCATE TABLE Words;", conn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+
+            Console.WriteLine("Starte Einfügen...");
+
+            using (var tran = conn.BeginTransaction())
+            {
+                int inserted = 0;
+
+                while (inserted < wordCount)
+                {
+                    int currentBatch = Math.Min(batchSize, wordCount - inserted);
+                    var values = new List<string>(currentBatch);
+
+                    for (int i = 0; i < currentBatch; i++)
+                    {
+                        string word = GenerateRandomWord(10, rnd);
+                        values.Add($"('{word}')");
+                    }
+
+                    string sql = $"INSERT INTO Words (value) VALUES {string.Join(",", values)};";
+                    using (var cmd = new MySqlCommand(sql, conn, tran))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+
+                    inserted += currentBatch;
+                    Console.WriteLine($"{inserted:N0} Wörter eingefügt...");
+                }
+
+                tran.Commit();
+            }
+        }
+
+        Console.WriteLine($"Fertig! {wordCount:N0} Wörter wurden eingefügt.");
+    }
+
+    static string GenerateRandomWord(int length, Random rnd)
+    {
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        StringBuilder sb = new StringBuilder(length);
+        for (int i = 0; i < length; i++)
+        {
+            sb.Append(chars[rnd.Next(chars.Length)]);
+        }
+        return sb.ToString();
+    }
+}
